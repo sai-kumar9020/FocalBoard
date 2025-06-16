559428ded361938e7c90d2eee14b089a7d0b4eef
sai-kumar9020 <100404258+sai-kumar9020@users.noreply.github.com>
2025-06-16 11:24:01 +0530

    Updated Backend

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "hcltrainings"
-    key    = "terraform.tfstate"
+    key    = "uc-10/terraform.tfstate"
     region = "us-east-1"
     encrypt      = true
     use_lockfile = true
